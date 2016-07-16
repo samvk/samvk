23ad5fab525480014a93e3e980723fa27e7acb5c
samvnkauffman-pc <samvnkauffman@gmail.com>
2016-07-16 02:18:52 -0400

    font-size change

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php require "include/head.php" ?>
+
+<body class="_404">
+    
+    <nav>
+        <div class="container">
+            <div class="relative">
+                <a class="autoscroll nav__logo" href="/">SamVK</a>
+            </div>
+        </div>
+    </nav>
+
+    <section class="_404__container text-center">
+        <h1>404</h1>
+        <hr class="redline">
+        <p class="italic">Page not found</p>
+        <a class="button" href="/"><span class="fa fa-arrow-circle-left"></span> Go Home</a>
+    </section>
+</body>
